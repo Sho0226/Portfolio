25aedffa897740756686691e654f587594fe7854
Sho0226 <s1F102201616@iniad.org>
2024-07-27 13:17:19 +0900

    fix: cursor+a

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,10 @@ import styles from './Navbar.module.css';
 
 const Navbar = forwardRef<HTMLDivElement>((props, ref) => (
   <div ref={ref} className={styles.navbar}>
-    <div className={styles.logo}>My Portfolio</div>
-    <div className={styles.navLinks}>
+    <div className={styles.logo}>
       <Link href="/">Portfolio</Link>
+    </div>
+    <div className={styles.navLinks}>
       <Link href="/about">About</Link>
       <Link href="/skills">Skills</Link>
       <Link href="/works">Works</Link>
